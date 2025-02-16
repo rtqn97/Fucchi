d2d807d4bb26caa2bc0f3eb6d3609b10d01d69d8
rtqn97 <lalitamgr12@gmail.com>
2025-02-16 17:55:55 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.querySelector(".burning-rose").style.animation = "burn 5s linear forwards";
+    }, 500);
+});
